884198599a2c427bd62d963d6aea9fb28d66e33d
kangtao <kevintao93@gmail.com>
2015-01-24 10:48:01 -0800

    map profile controller added

---
+++ MapProfileController.h
@@ -0,0 +1,13 @@
+//
+//  MapProfileController.h
+//  CSSAMon
+//
+//  Created by TK on 1/17/15.
+//  Copyright (c) 2015 Ruiqing Qiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapProfileController : UIViewController
+
+@end
---
+++ MapProfileController.m
@@ -0,0 +1,13 @@
+//
+//  MapProfileController.m
+//  CSSAMon
+//
+//  Created by TK on 1/17/15.
+//  Copyright (c) 2015 Ruiqing Qiu. All rights reserved.
+//
+
+#import "MapProfileController.h"
+
+@implementation MapProfileController
+
+@end
